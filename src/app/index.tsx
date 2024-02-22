0d5394b9173ef8598fb141d16d49dabc0f65f30e
leolikestocode <leonardofpereira25@gmail.com>
2024-02-22 11:53:05 -0300

    Init RN expor

---
+++ src/app/index.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from "react-native"
+
+export default function Home() {
+  return (
+    <View className="bg-red-500 pt-8">
+      <Text className="text-white text-2xl">Hello RN</Text>
+    </View>
+  )
+}
