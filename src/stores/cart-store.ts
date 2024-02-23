8b0b8d5019e5ee9ea6e7d6716d49d36aae0e0944
leolikestocode <leonardofpereira25@gmail.com>
2024-02-23 13:50:13 -0300

    Project finished - NLW RN

---
+++ src/stores/cart-store.ts
@@ -0,0 +1,42 @@
+import { ProductProps } from "@/utils/data/products"
+import { create } from "zustand"
+import * as cartInMemory from "./helpers/cart-in-memory"
+import { createJSONStorage, persist } from "zustand/middleware"
+import AsyncStorage from "@react-native-async-storage/async-storage"
+
+// AsyncStorage.clear(); // well it's self explanatory
+
+export type ProductCartProps = ProductProps & {
+  quantity: number
+}
+
+type StateProps = {
+  products: ProductCartProps[]
+  add: (product: ProductProps) => void
+  remove: (product: ProductProps) => void
+  clear: () => void
+}
+
+export const useCartStore = create(
+  persist<StateProps>(
+    (set) => ({
+      products: [],
+
+      add: (product: ProductProps) =>
+        set((state) => ({
+          products: cartInMemory.add(state.products, product),
+        })),
+
+      remove: (product: ProductProps) =>
+        set((state) => ({
+          products: cartInMemory.remove(state.products, product),
+        })),
+
+      clear: () => set(() => ({ products: [] })),
+    }),
+    {
+      name: "nlw-expert:cart",
+      storage: createJSONStorage(() => AsyncStorage),
+    }
+  )
+)
